4602b5211c17df9a13938c823cd4e19e398ba8a8
Alexander Bunn <alexander@applyinnovations.com.au>
2026-08-23 16:17:43 +1000

    Refresh pinned vulnerability database

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     # The canonical vuln.go.dev hostname rejects GitHub-hosted runner IPs;
     # pin the official bulk object by its immutable GCS generation as well as
     # the Nix content hash recorded in flake.lock.
-    url = "https://storage.googleapis.com/download/storage/v1/b/go-vulndb/o/vulndb.zip?alt=media&generation=1787088262759230";
+    url = "https://storage.googleapis.com/download/storage/v1/b/go-vulndb/o/vulndb.zip?alt=media&generation=1787345732225399";
     flake = false;
   };
 
